221c95c126063e9f7222cf638505419fdacf23fb
ljkhyeong <jolri24@gmail.com>
2025-07-01 21:59:34 +0900

    Refactor : Merge Dto
    CartItemDto, OrderItemDto 구조가 똑같으므로 병합

--- src/main/java/shop/personal/happyGallery/dto/ItemDto.java
+++ src/main/java/shop/personal/happyGallery/dto/ItemDto.java
@@ -1,11 +1,8 @@
 package shop.personal.happyGallery.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import shop.personal.happyGallery.model.Product;
 import shop.personal.happyGallery.model.embeded.Money;
 
-public record CartItemDto(
+public record ItemDto(
 	Long productId,
 	String name,
 	Money price,
