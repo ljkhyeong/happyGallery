823223a51a0f2b4520d09850a590b68fe95092f0
LJK <jolri24@gmail.com>
2026-01-24 15:11:17 +0900

    init : project init

--- src/main/java/shop/personal/happyGallery/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package shop.personal.happyGallery.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import shop.personal.happyGallery.model.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- src/main/java/shop/personal/happyGallery/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package shop.personal.happyGallery.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import shop.personal.happyGallery.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
