daede7cf153121b112603b28b757858fa45899e6
LJK <jolri24@gmail.com>
2026-08-20 21:58:17 +0900

    Refactor: Spring Retry 설정을 루트 구성에 통합

--- bootstrap/src/main/java/com/personal/happygallery/HappygalleryApplication.java
+++ bootstrap/src/main/java/com/personal/happygallery/HappygalleryApplication.java
@@ -3,9 +3,12 @@ package com.personal.happygallery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.core.Ordered;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableRetry(order = Ordered.LOWEST_PRECEDENCE - 1)
 public class HappygalleryApplication {
 
 	public static void main(String[] args) {
