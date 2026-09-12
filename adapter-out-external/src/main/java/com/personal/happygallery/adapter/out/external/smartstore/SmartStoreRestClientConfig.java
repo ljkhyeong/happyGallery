a088dbec6e0b0666bae476f26ef18a44b5a4b23f
LJK <jolri24@gmail.com>
2026-09-12 14:13:23 +0900

    Fix: 스마트스토어 주소 변경 응답의 성공 오판 방지

--- adapter-out-external/src/main/java/com/personal/happygallery/adapter/out/external/smartstore/SmartStoreRestClientConfig.java
+++ adapter-out-external/src/main/java/com/personal/happygallery/adapter/out/external/smartstore/SmartStoreRestClientConfig.java
@@ -5,8 +5,10 @@ import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientResponseException;
 
 @Configuration(proxyBeanMethods = false)
 class SmartStoreRestClientConfig {
@@ -30,6 +32,11 @@ class SmartStoreRestClientConfig {
         return builder
                 .baseUrl(properties.baseUrl())
                 .requestFactory(new HttpComponentsClientHttpRequestFactory(httpClient))
+                .defaultStatusHandler(HttpStatusCode::is3xxRedirection, (request, response) -> {
+                    throw new RestClientResponseException(
+                            "스마트스토어 API가 주소 변경을 요청했습니다. 연동 주소를 확인해 주세요.",
+                            response.getStatusCode().value(), "", null, null, null);
+                })
                 .build();
     }
 }
